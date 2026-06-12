a977ca777e122f57d39ffc714c4f41e669050095
Rogher Adriano Soares <rogher_adriano@hotmail.com>
2026-06-11 21:49:59 -0300

    Corrige caminho do arquivo de validacoes

--- validacoes.js
+++
@@ -1,8 +0,0 @@
-function validarRetirada(estoque, quantidade) {
-  // A quantidade de retirada deve ser maior que 0 e não pode ultrapassar o estoque atual
-  return quantidade > 0 && quantidade <= estoque;
-}
-
-module.exports = {
-  validarRetirada,
-};
