6e34eb44aecf7a9b5a10b32e8b3aab2390ed2c50
Rogher Adriano Soares <rogher_adriano@hotmail.com>
2026-06-11 21:38:13 -0300

    refactor: atualiza textos finais da interface

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ import {
 const API_URL = "https://6a18c6de23c3626470ac0536.mockapi.io/api/v1/materiais";
 
 export default function App() {
-  // --- Estados da Aplicação (Os alunos implementarão aqui) ---
+  // --- Estados da Aplicação ---
   const [nome, setNome] = useState("");
   const [quantidade, setQuantidade] = useState("");
   const [materiais, setMateriais] = useState([]);
@@ -23,7 +23,7 @@ export default function App() {
   const [cadastrando, setCadastrando] = useState(false);
   const [atualizando, setAtualizando] = useState(false);
 
-  // --- Funções de Requisição e Efeitos (Os alunos implementarão aqui) ---
+  // --- Funções de Requisição e Efeitos ---
   const buscarMateriais = async (atualizacaoManual = false) => {
     try {
       if (atualizacaoManual) {
@@ -140,13 +140,9 @@ export default function App() {
     >
       <Text style={styles.title}>Almoxarifado - Enfermagem</Text>
 
-      {/* Breve descrição do projeto inserida abaixo */}
       <Text style={styles.description}>
-        Este template servirá para desenvolver o projeto responsável por
-        modernizar o controle de insumos médicos do almoxarifado. Através desta
-        interface conectada à API, é possível realizar o inventário em tempo
-        real, cadastrar novos materiais e registrar baixas de estoque de forma
-        ágil e segura.
+        Controle mobile de materiais do almoxarifado. Consulte o inventário
+        atualizado e cadastre novos insumos de forma rápida e segura.
       </Text>
 
       <Text style={styles.label}>Nome do material</Text>
